282afb35a44c625b7eaf2f08d1313053c1520a67
Ero Aguilar <eroaguilar7@gmail.com>
2022-03-29 14:14:45 +0800

    Update db password for connection to mysql.

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "mypedisp_user", "Prime@73147{", "mypedisp_db");
+$con=mysqli_connect("localhost", "mypedisp_user", "Prime@73147", "mypedisp_db");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
